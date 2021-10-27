f5f62e28a1ca8e5f5bd706c38b8e29278d5872e9
Dep321 <91454349+Dep321@users.noreply.github.com>
2021-10-27 14:35:50 +0700

    Add files via upload

---
+++ Review.java
@@ -0,0 +1,85 @@
+//Java Review Q1
+//import java.util.Scanner;
+//public class Review {
+  //  public static void main(String[] args) {
+    //int firstNumber;
+    //int secondNumber;
+
+    //Scanner input = new Scanner(System.in);
+    //System.out.println("Write down two numbers to get the sum of");
+    //firstNumber = input.nextInt();
+    //secondNumber = input.nextInt();
+    //System.out.println(firstNumber + secondNumber);
+    //}
+//}
+
+//Java Review Q2
+//import java.util.Scanner;
+//public class Review {
+  //  public static void main(String[] args) {
+    //    int firstNumber;
+      //  int secondNumber;
+//
+  //      Scanner input = new Scanner(System.in);
+    //    System.out.println("Write down two numbers to get the product of");
+      //  firstNumber = input.nextInt();
+        //secondNumber = input.nextInt();
+        //System.out.println(firstNumber * secondNumber);
+    //}
+//}
+
+//Java Review Q3
+//import java.util.Scanner;
+//public class Review {
+  //  public static void main(String[] args) {
+    //    int firstNumber;
+
+      //  Scanner input = new Scanner(System.in);
+       // System.out.println("Write down one number to get the table of 10 of that said number");
+        //firstNumber = input.nextInt();
+
+//        System.out.println(firstNumber * 0);
+//        System.out.println(firstNumber * 1);
+//        System.out.println(firstNumber * 2);
+//        System.out.println(firstNumber * 3);
+//        System.out.println(firstNumber * 4);
+//        System.out.println(firstNumber * 5);
+//        System.out.println(firstNumber * 6);
+//        System.out.println(firstNumber * 7);
+//        System.out.println(firstNumber * 8);
+//        System.out.println(firstNumber * 9);
+//       System.out.println(firstNumber * 10);
+//    }
+//}
+
+//Java Review Q4
+//import java.util.Scanner;
+//public class Review {
+  //  public static void main(String[] args) {
+    //    System.out.println("Insert three integers to find your average");
+    //    int firstNumber;
+    //    int secondNumber;
+    //    int thirdNumber;
+    //    Scanner input = new Scanner(System.in);
+    //    firstNumber = input.nextInt();
+    //    secondNumber = input.nextInt();
+    //    thirdNumber = input.nextInt();
+    //    double averageNumber;
+    //    averageNumber= ((firstNumber+secondNumber+thirdNumber)/3);
+    //    System.out.println("This is the average number of your inputs:" + " " + averageNumber);
+   // }
+//}
+
+//Java Review 5
+//import java.util.Scanner;
+//public class Review {
+  //  public static void main(String[] args) {
+  //      System.out.println("Input the temperature you want to convert to from Fahrenheit to Celsius");
+  //      double Fahrenheit;
+  //      double Celsius;
+  //      Scanner input = new Scanner(System.in);
+  //      Fahrenheit = input.nextDouble();
+  //      Celsius = ((Fahrenheit - 32)*5/9);
+  //      System.out.println("This is the temperature you requested in Celsius" + " " + Celsius + "ºC");
+  //  }
+//}
