ae4ea5139ac5006b24d6661bc607455d4e932c05
Dep321 <91454349+Dep321@users.noreply.github.com>
2022-01-08 23:15:29 +0700

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,35 @@
+//WHILE LOOP
+//public class loops {
+//    public static void main(String[] args) {
+//
+//        int a  = 0;
+//
+//        //while(a<100) {System.out.println(a++);
+//        }
+//        System.out.println("Loop Finished");
+//    }
+//}
+
+//FOR LOOP
+//public class loops {
+//        public static void main(String[] args) {
+//
+//            for (int a =0; a<100; a++) {
+//                System.out.println (a);
+//            }
+//            System.out.println("Code Finished");
+//        }
+//}
+
+//DO WHILE LOOP
+//public class loops {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        do {
+//            System.out.println("Hello World");
+//        } while (a < 10); {
+//            System.out.println("The code is working until it stopped");
+//        }
+//
+//    }
+//}
